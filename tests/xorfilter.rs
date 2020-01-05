d14fb44bf912dc775c8bf86c8349ac0fb36ce89e
prataprc <prataprc@gmail.com>
2020-01-05 19:59:54 +0530

    test: cleanup file.

--- tests/xorfilter.rs
+++ tests/xorfilter.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use rand::{prelude::random, rngs::SmallRng, Rng, SeedableRng};
 use xorfilter::Xor8;
 
@@ -16,14 +18,28 @@ fn generate_filter() -> Xor8 {
     *Xor8::populate(&keys)
 }
 
+struct TestFile(String);
+
+impl Drop for TestFile {
+    fn drop(&mut self) {
+        fs::remove_file(&self.0).ok();
+    }
+}
+
 #[test]
 fn test_same_filter_encode_decode() {
-    let file_path = "test_encode.bin";
+    let file_path = TestFile("test_encode.bin".to_string());
     let filter = generate_filter();
 
-    filter.write_file(file_path).expect(&format!("Write to {} failed", file_path));
-    let filter_read = Xor8::read_file(file_path).expect(&format!("Read from {} failed", file_path));
-    assert_eq!(filter_read, filter, "Filter unequals after encode and decode");
+    filter
+        .write_file(&file_path.0)
+        .expect(&format!("Write to {} failed", file_path.0));
+    let filter_read =
+        Xor8::read_file(&file_path.0).expect(&format!("Read from {} failed", file_path.0));
+    assert_eq!(
+        filter_read, filter,
+        "Filter unequals after encode and decode"
+    );
 
     let filter_second = generate_filter();
     assert_ne!(
