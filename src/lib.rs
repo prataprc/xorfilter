961a898f4619d3f58dc3f905dac736bfbdb62cd5
prataprc <prataprc@gmail.com>
2020-03-19 17:30:03 +0530

    improve test cases.

--- src/lib.rs
+++ src/lib.rs
@@ -481,7 +481,7 @@ mod tests {
         println!("seed {}", seed);
         let mut rng = SmallRng::from_seed(seed.to_le_bytes());
 
-        let testsize = 100_000;
+        let testsize = 1_000_000;
         let mut keys: Vec<u64> = Vec::with_capacity(testsize);
         keys.resize(testsize, Default::default());
         for key in keys.iter_mut() {
@@ -516,6 +516,45 @@ mod tests {
 
     #[test]
     fn test_basic2() {
+        let seed: u128 = random();
+        println!("seed {}", seed);
+        let mut rng = SmallRng::from_seed(seed.to_le_bytes());
+
+        let testsize = 1_000_000;
+        let mut keys: Vec<u64> = Vec::with_capacity(testsize);
+        keys.resize(testsize, Default::default());
+        for key in keys.iter_mut() {
+            *key = rng.gen();
+        }
+
+        let filter = {
+            let mut filter = Xor8::<RandomState>::new();
+            filter.populate_keys(&keys);
+            filter.build();
+            filter
+        };
+
+        for key in keys.into_iter() {
+            assert!(filter.contains_key(key), "key {} not present", key);
+        }
+
+        let (falsesize, mut matches) = (10_000_000, 0_f64);
+        let bpv = (filter.finger_prints.len() as f64) * 8.0 / (testsize as f64);
+        println!("bits per entry {} bits", bpv);
+        assert!(bpv < 10.0, "bpv({}) >= 10.0", bpv);
+
+        for _ in 0..falsesize {
+            if filter.contains(rng.gen::<u64>()) {
+                matches += 1_f64;
+            }
+        }
+        let fpp = matches * 100.0 / (falsesize as f64);
+        println!("false positive rate {}%", fpp);
+        assert!(fpp < 0.40, "fpp({}) >= 0.40", fpp);
+    }
+
+    #[test]
+    fn test_basic3() {
         let mut seed: u64 = random();
         println!("seed {}", seed);
 
