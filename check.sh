76d10744eea0310deb529cdb08fdfa1d57a313fa
prataprc <prataprc@gmail.com>
2021-01-22 21:21:22 +0530

    check script.

--- check.sh
+++ check.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
-cargo +stable build
+# cargo +stable build
 cargo +nightly build
 
-cargo +stable doc
+# cargo +stable doc
 cargo +nightly doc
 
 cargo +nightly clippy --all-targets --all-features
 
 cargo +nightly test;
-cargo +stable test
+# cargo +stable test
 
 cargo +nightly bench;
